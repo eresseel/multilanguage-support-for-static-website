4297bc7d3331d58c121a4a67f93b086372ee83ef
lberes <laszlo.beres@nng.com>
2023-01-27 22:06:41 +0100

    [OS-14] Add cookie content in other languages

---
+++ assets/js/cookie.js
@@ -0,0 +1,27 @@
+let languages = [
+    en = {
+        content: "By using our website, you approve the use of cookies.",
+        title: "Acceptance of cookies",
+        accept: "Accept"
+    },
+    de = {
+        content: "Durch die Nutzung unserer Website stimmen Sie der Verwendung von Cookies zu.",
+        title: "Annahme von Cookies",
+        accept: "Akzeptieren"
+    },
+    fr = {
+        content: "En utilisant notre site Web, vous acceptez l'utilisation de cookies.",
+        title: "Acceptation des cookies",
+        accept: "Accepter"
+    },
+    hu = {
+        content: "Weboldalunk használatával jóváhagyod a sütik használatát.",
+        title: "Süti elfogadása",
+        accept: "Elfogadom"
+    },
+    it = {
+        content: "Utilizzando il nostro sito Web, accetti l'uso dei cookie.",
+        title: "Accettazione dei cookie",
+        accept: "Accettazione dei cookie"
+    }
+];
