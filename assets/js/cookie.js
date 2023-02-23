4d321965c92a57794049f9c019d53b28fbafcb94
lberes <laszlo.beres@nng.com>
2023-02-23 15:37:32 +0100

    [OS-14] Fix language select

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,35 +1,69 @@
-let languages = [
-    en = {
+let languages = {
+    en: {
         head: "Cookies",
         content: "By using our website, you approve the use of cookies.",
         title: "Acceptance of cookies",
         accept: "Accept"
     },
-    de = {
+    de: {
         head: "Cookies",
         content: "Durch die Nutzung unserer Website stimmen Sie der Verwendung von Cookies zu.",
         title: "Annahme von Cookies",
         accept: "Akzeptieren"
     },
-    fr = {
+    fr: {
         head: "Cookies",
         content: "En utilisant notre site Web, vous acceptez l'utilisation de cookies.",
         title: "Acceptation des cookies",
         accept: "Accepter"
     },
-    hu = {
+    hu: {
         head: "Sütik",
         content: "Weboldalunk használatával jóváhagyod a sütik használatát.",
         title: "Süti elfogadása",
         accept: "Elfogadom"
     },
-    it = {
+    it: {
         head: "Cookies",
         content: "Utilizzando il nostro sito Web, accetti l'uso dei cookie.",
         title: "Accettazione dei cookie",
         accept: "Accettare"
     }
-];
+};
+
+let defaultLanguage = "hu";
+let browserLanguage = navigator.language || navigator.userLanguage;
+browserLanguage = browserLanguage.substring(0, 2)
+
+if (languages[browserLanguage]) {
+    setCookie("language", browserLanguage, 2);
+} else {
+    setCookie("language", defaultLanguage, 2);
+}
+
+console.log(document.cookie);
+let cookieHtml = "";
+
+Object.entries(languages).forEach(([key, value]) => {
+    if (key == browserLanguage.substring(0, 2)) {
+        cookieHtml += `
+        <div>
+            <blockquote class="blockquote">
+                <a>`+ languages[key]["head"] + `</a>
+            </blockquote>
+        </div>
+        <div>
+            <p>`+ languages[key]["content"] + `</p>
+        </div>
+        <div class="btn btn-primary">
+            <a onclick="cookieAccept();" title="`+ languages[key]["title"] + `">` + languages[key]["accept"] + `</a>
+        </div>
+    `
+    }
+    // console.log(key, value.head);
+});
+
+$('#cookieAccept').html(cookieHtml);
 
 function hideCookie() {
     $("#cookieAccept").hide("slow", function () {
